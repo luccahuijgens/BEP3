11eb83cb2cc31f23f398cab70d74bc24b03c4760
lhuijgens <lucca.huijgens@student.hu.nl>
2019-04-26 17:37:28 +0200

    initial commit

---
+++ src/main/java/com/example/project/MongoSaver.java
@@ -0,0 +1,53 @@
+package com.example.project;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoSaver {
+	
+	public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
+		String userName = "lhuijgens";
+		String password = "lhuijgens";
+		String database = "testdatabase";
+		
+		boolean success = true;
+
+		MongoClientURI uri=new MongoClientURI("mongodb+srv://"+userName+":"+password+"@cluster0-3q7uo.mongodb.net/test?retryWrites=true");		
+		
+		try (MongoClient mongoClient=new MongoClient(uri);) {
+			
+			MongoDatabase db = mongoClient.getDatabase( database );
+			
+			MongoCollection<Document> c = db.getCollection("email");
+			
+			Document  doc = new Document ("to", to)
+			        .append("from", from)
+			        .append("subject", subject)
+			        .append("text", text)
+			        .append("asHtml", html);
+			c.insertOne(doc);
+		} catch (MongoException mongoException) {
+			System.out.println("XXXXXXXXXXXXXXXXXX ERROR WHILE SAVING TO MONGO XXXXXXXXXXXXXXXXXXXXXXXXXX");
+			mongoException.printStackTrace();
+			success = false;
+		}
+		
+		return success;
+ 		
+	}
+
+}
